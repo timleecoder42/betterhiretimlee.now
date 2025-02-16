ab7a6d977fe4a80f1df5b091128025b8e20b9c81
timleecoder42 <timleecoder42@gmail.com>
2025-02-16 11:44:47 +0800

    perf: try to fix middleware #6

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,7 @@ import { routing } from '@/i18n/routing';
 
 export default createMiddleware(routing);
 
-// Specify each path explicitly for static analysis
 export const config = {
-  matcher: [
-    // Match all pathnames except for
-    // - /api (API routes)
-    // - /_next (Next.js internals)
-    // - /static (inside /public)
-    // - .*\\..*\$ (files with extensions like favicon.ico)
-    '/((?!api|_next|static|.*\\..*$).*)',
-    // Match all paths that start with supported locales
-    '/',
-    '/en/:path*',
-    '/zh/:path*',
-    '/ja/:path*',
-  ],
+  // Match only internationalized pathnames
+  matcher: ['/', '/(en|zh|ja)/:path*'],
 };
